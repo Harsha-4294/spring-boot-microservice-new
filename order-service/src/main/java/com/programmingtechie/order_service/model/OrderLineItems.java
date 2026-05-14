b44d8e44224056bbda8b70e927d307bcf26078ef
Harsha <gawandeharsha09@gmail.com>
2026-05-14 13:24:59 +0530

    Initial commit

---
+++ order-service/src/main/java/com/programmingtechie/order_service/model/OrderLineItems.java
@@ -0,0 +1,25 @@
+package com.programmingtechie.order_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//import javax.persistence.*;
+
+@Entity
+@Table(name = "t_order_line_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderLineItems {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Double price;
+    private Integer quantity;
+}
---
+++ order-service/src/main/java/com/programmingtechie/order_service/service/OrderService.java
@@ -0,0 +1,51 @@
+package com.programmingtechie.order_service.service;
+
+import com.programmingtechie.order_service.dto.OrderLineItemsDto;
+import com.programmingtechie.order_service.dto.OrderRequest;
+import com.programmingtechie.order_service.model.Order;
+import com.programmingtechie.order_service.model.OrderLineItems;
+import com.programmingtechie.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+    @Autowired
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .collect(Collectors.toList());
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        orderRepository.save(order);
+
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+
+        OrderLineItems orderLineItems = new OrderLineItems();
+
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+
+}
